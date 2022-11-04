d2b5b9809c4ec569e8a4da84a6413d7866f37d23
pr0pr09er <113778429+pr0pr09er@users.noreply.github.com>
2022-11-04 22:24:50 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,202 @@
+"use scrict"
+
+// function buttonClick() {
+//     var input = document.getElementById('input');
+// 	   input.value = '!!!';
+
+// function buttonClick() {
+//     var input = document.getElementById('input');
+// 	   alert(input.value);
+// }
+
+// function buttonClick() {
+//     var input = document.getElementById('input');
+// 	   var number = Number(input.value);
+// 	   var square = number*number;
+// 	   alert(square);
+// }
+
+// function buttonClick() {
+// 	   let input = document.getElementById('input');
+//     let input1 = document.getElementById("input1");
+// 	   let value = input.value;
+//     input.value = input1.value;
+//     input1.value = value;
+//     alert(number ** 2);
+// }
+
+// function buttonClick() {
+//     elem.value = 'Новый текст кнопки';
+// }
+
+// function buttonClick() {
+//     document.getElementById('input').style.color = 'red';
+
+// }
+// //Заблокирует элемент
+// function button1Click() {
+//     var input = document.getElementById('input');
+//     input.disabled = true;
+// }
+    
+// //Отблокирует элемент
+// function button2Click() {
+//     var input = document.getElementById('input');
+//     input.disabled = false;
+// }
+
+// function buttonClick() {
+//     alert("Hello");
+// }
+    
+// function buttonHover() {
+//     alert("Hello");
+// }
+
+// function buttonDoubleClick() {
+//     alert("Hello");
+// }
+
+// function buttonOut() {
+//     alert("Hello");
+// }
+
+// function buttonClick() {
+//     let inputValue = input.value;
+//     alert(inputValue);
+// }
+
+// function buttonClick() {
+//     input.value = "Я поменял твой текст";
+// }
+
+// function buttonClick() {
+//     photo.src = "https://picsum.photos/id/0/5616/3744";
+// }
+
+// function buttonClick(input) {
+//     while (true) {
+//         alert(input.value);
+//     }
+// }
+
+
+// function stop(input) {
+//     input.value = "Еще раз поменял";
+// }
+// function clicks(input) {
+//     input.value = "Я поменял твой текст";
+// }
+
+// function clicks(submit) {
+//     submit.value = "Ку-ку";
+// }
+
+// function clicks(submit) {
+//     submit.disabled = true;
+//     submit.value = "Больше нельзя нажать!";
+// }
+
+// function buttonOut(image) {
+//     image.src = 'assets/img/1.jpg';
+// }
+// function buttonHover(image) {
+//     image.src = 'assets/img/2.jpg';
+// }
+
+// function clicks(){
+//     let elem = document.getElementById('input');
+//     elem.style.color = 'red';
+//     elem.style.width = '300px';
+// }
+
+// let input = document.getElementById("input");
+// function inputNotVisible() {
+//     input.style.display = "none";
+// }
+// function inputVisible() {
+//     input.style.display = "inline-block";
+// }
+
+// function inputTransform() {
+//     let input = document.getElementById("input");
+//     input.style.color = "red";
+//     input.value = "Ой, я поменял твой текст и css!";
+//     input.style.width = '300px';
+//     input.style.height = '50px';
+//     input.style.borderRadius = '10px';
+// }
+
+// function block(){
+//     var block = document.getElementById('block');
+//     var unblock = document.getElementById('unblock');
+//     block.value = 'О, теперь на меня больше не нажать!';
+//     block.disabled = true;
+//     unblock.style.display = 'block';
+// }
+// function unblock(){
+//     var block = document.getElementById('block');
+//     var unblock = document.getElementById('unblock');
+//     block.value = 'О, на меня снова можно нажимать!';
+//     block.disabled = false;
+//     unblock.style.display = 'none';
+// }
+
+// let counter = 0
+// function clickCounter() {
+//     document.getElementById("button").innerHTML = ++counter;
+// }
+
+// function clicks() {
+//     let input = document.getElementById("input");
+//     input.style.position = "absolute";
+//     input.style.left = "200px";
+//     input.value = "Теперь я плаваю справа!";
+// }
+
+// function classView() {
+//     let input = document.getElementById("input");
+//     input.value = "Мои css классы: " + input.className;
+// }
+
+// function clicks() {
+//     let input1 = document.getElementById("input1");
+//     let input2 = document.getElementById("input2");
+//     let temp = input1.value;
+//     input1.value = input2.value;
+//     input2.value = temp;
+// }
+
+// function clicks() {
+//     let number = document.getElementById("input1").value;
+//     let input = document.getElementById("input2");
+//     input.value = number ** 2;
+// }
+
+// function clicks() {
+//     let input1 = document.getElementById('input1');
+//     if (!isNaN(input1.value)) {
+//         let input2 = document.getElementById('input2');
+//         input2.value = input1.value ** 2;
+//     } else {
+//         alert("Это не число");
+//         input1.value = '';
+//     }
+// }
+
+// function pOver() {
+//     let p = document.getElementById("p");
+//     p.style.cursor = "text";
+// }
+
+// function clicks() {
+//     let button = document.getElementById("button");
+//     button.style.cursor = "not-allowed";
+//     button.disabled = true;
+// }
+
+
+// function addElement(element) {
+//     let input = document.getElementById("input");
+//     input.value = input.value + element;
+// }
